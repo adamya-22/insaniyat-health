082df324e6fe0418c2edd2d40deb169ad3b40fe8
adamya-22 <104303051+adamya-22@users.noreply.github.com>
2024-03-23 17:15:44 +0530

    se4

--- api/sendEmail.js
+++ api/sendEmail.js
@@ -24,6 +24,7 @@ export default async function handler(req, res) {
     console.log('Email sent successfully!');
     res.status(200).json({ message: 'Email sent successfully!' });
   } catch (error) {
+    console.log("fuck you");
     console.error('Error sending email:', error.message);
     res.status(500).json({ message: 'Failed to send email. Please try again later.' });
   }
