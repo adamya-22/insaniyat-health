bf65ae1c3f3fa7a5ac21a9f82e0e7c9c965a462f
adamya-22 <104303051+adamya-22@users.noreply.github.com>
2024-03-23 17:29:37 +0530

    deleted packages

--- api/sendEmail.js
+++ api/sendEmail.js
@@ -1,30 +1,25 @@
-import nodemailer from 'nodemailer';
+import sgMail from '@sendgrid/mail';
 
 export default async function handler(req, res) {
   const { name, email, subject, message } = req.body;
 
-  // Create a Nodemailer transporter
-  let transporter = nodemailer.createTransport({
-    service: 'Gmail',
-    auth: {
-      user: 'adamyachaturvedi22@gmail.com',
-      pass: 'met22met',
-    },
-  });
+  // Set SendGrid API key
+  sgMail.setApiKey('your_sendgrid_api_key');
+
+  const msg = {
+    to: 'recipient@example.com',
+    from: email,
+    subject: subject,
+    text: `Name: ${name}\nEmail: ${email}\nMessage: ${message}`,
+  };
 
   try {
-    // Send email
-    await transporter.sendMail({
-      from: email,
-      to: 'adamyachaturvedi22@gmail.com',
-      subject: subject,
-      text: `Name: ${name}\nEmail: ${email}\nMessage: ${message}`,
-    });
+    // Send email using SendGrid API
+    await sgMail.send(msg);
 
     console.log('Email sent successfully!');
     res.status(200).json({ message: 'Email sent successfully!' });
   } catch (error) {
-    console.log("fuck you");
     console.error('Error sending email:', error.message);
     res.status(500).json({ error });
   }
